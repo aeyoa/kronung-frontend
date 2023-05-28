25c4f67512669b3143806d4fe02a028741055a36
Arseny Kogan <arsenykogan@gmail.com>
2023-05-28 17:34:38 +0700

    oh finally sticky

---
+++ components/Sticky.js
@@ -0,0 +1,70 @@
+import { sections } from "@/content/homepage"
+import bp from "@/styles/breakpoints"
+import { useViewportSize } from "@mantine/hooks"
+import Image from "next/image"
+import { useEffect, useRef } from "react"
+
+const ASPECT_RATIO = 0.64
+const TABLET = 640
+const COLUMN_WIDTH = 0.7
+
+export default function Sticky({ setStickyBottom }) {
+  const { width, height } = useViewportSize()
+  const ref = useRef()
+  useEffect(() => {
+    setStickyBottom(
+      ref.current.getBoundingClientRect().top +
+        window.scrollY +
+        ref.current.getBoundingClientRect().height
+    )
+  })
+  let w, h, top
+  if (width < TABLET) {
+    w = width
+    h = w * ASPECT_RATIO
+    top = 150
+  } else {
+    const areaWidth = width * COLUMN_WIDTH - 88
+    const areaHeight = height - 88 - 22 - 44
+    if (areaHeight / areaWidth > ASPECT_RATIO) {
+      /* Fits vertically */
+      w = areaWidth
+      h = w * ASPECT_RATIO
+      top = height - h - 44
+    } else {
+      /* Doesnt fit */
+      h = areaHeight
+      w = h / ASPECT_RATIO
+      top = height - h - 44
+    }
+  }
+
+  return (
+    <div
+      ref={ref}
+      css={bp({
+        zIndex: 6,
+        // This should be calculated
+        marginTop: [200, 200],
+        position: "sticky",
+        // height: "calc(100vw * 0.7 * 0.64)",
+        // top: [250, "calc(100vh - (100vw * 0.7 * 0.64))"],
+        marginLeft: "auto",
+        marginRight: "auto",
+        height: h,
+        width: w,
+        top,
+      })}>
+      <Image
+        src={sections[0].image}
+        style={{
+          width: "100%",
+          height: "auto",
+          position: "absolute",
+          bottom: 0,
+        }}
+        alt="*"
+      />
+    </div>
+  )
+}
--- pages/test.js
+++ pages/test.js
@@ -1,36 +1,45 @@
 import Logo from "@/components/Logo"
+import Sticky from "@/components/Sticky"
 import { cover } from "@/content/homepage"
 import { sections } from "@/content/homepage"
 import bp from "@/styles/breakpoints"
 import { absoluteFill, relative } from "@/styles/mixins"
+import { useMediaQuery, useViewportSize } from "@mantine/hooks"
 import Image from "next/image"
 import { useEffect, useRef, useState } from "react"
 
 export default function test(params) {
-  const sticky = useRef()
-  const [sticked, setSticked] = useState(false)
+  // const sticky = useRef()
+  // const [sticked, setSticked] = useState(false)
+  const { width, height } = useViewportSize()
+  const [stickyBottom, setStickyBottom] = useState(999)
   const [shrink, setShrink] = useState(false)
+  const notMobile = useMediaQuery("(min-width: 640px)")
+  const stickyBackgroundTop = 150 + width * 0.64 - stickyBottom
   const onScroll = () => {
-    console.log(window.scrollY)
-    setShrink(window.scrollY > 855)
+    if (window.innerWidth >= 640) {
+      setShrink(window.scrollY > 855)
+    } else {
+      setShrink(false)
+    }
   }
   useEffect(_ => {
-    const observer = new IntersectionObserver(
-      ([e]) => {
-        setSticked(e.intersectionRatio == 1)
-      },
-      {
-        threshold: [1],
-      }
-    )
-    observer.observe(sticky.current)
+    // const observer = new IntersectionObserver(
+    //   ([e]) => {
+    //     setSticked(e.intersectionRatio == 1)
+    //   },
+    //   {
+    //     threshold: [1],
+    //   }
+    // )
+    // observer.observe(sticky.current)
     window.addEventListener("scroll", onScroll)
     return () => {
       window.removeEventListener("scroll", onScroll)
     }
   }, [])
   return (
-    <div>
+    <div css={bp({})}>
       <Logo fullwidth={!shrink}></Logo>
       <div css={bp({ ...relative })}>
         <div css={bp({ ...absoluteFill })}>
@@ -39,13 +48,15 @@ export default function test(params) {
               position: "sticky",
               width: shrink ? "70%" : "100%",
               marginLeft: "auto",
-              height: "100vh",
-              top: 0,
+              height: [stickyBottom - 88, "100vh"],
+              top: [stickyBackgroundTop + 88, 0],
               transition: "width 200ms ease",
+              zIndex: 5,
             })}>
             <div
               css={bp({
                 backgroundColor: "#F7F8F7",
+                backgroundColor: "#F0F0F0",
                 position: "absolute",
                 top: 0,
                 left: 22,
@@ -54,7 +65,7 @@ export default function test(params) {
               })}></div>
           </div>
         </div>
-        <div css={bp({ ...relative })}>
+        <div css={bp({ ...relative, zIndex: 6 })}>
           <div css={bp({ display: "flex" })}>
             <div css={bp({ padding: 44 })}>{cover.left}</div>
             <div css={bp({ padding: 44 })}>{cover.right}</div>
@@ -68,27 +79,22 @@ export default function test(params) {
               position: ["absolute", "relative"],
               width: ["100%", "auto"],
               height: ["100%", "auto"],
+              marginBottom: [0, 44],
             })}>
-            <div
-              ref={sticky}
-              css={bp({
-                height: "calc(100vw * 0.7 * 0.64)",
-                marginTop: 200,
-                // backgroundColor: "#def",
-                border: "1px solid pink",
-                position: "sticky",
-                top: [250, "calc(100vh - (100vw * 0.7 * 0.64))"],
-              })}>
-              <Image
-                src={sections[0].image}
-                style={{ width: "100%", height: "100%", position: "absolute", bottom: 0 }}
-              />
-            </div>
+            <Sticky setStickyBottom={setStickyBottom} />
           </div>
-          <div css={bp({ flexBasis: "30%", paddingTop: [250, 0], zIndex: 10 })}>
+          <div css={bp({ flexBasis: "30%", paddingTop: 0, zIndex: 10 })}>
             {sections.map((section, index) => {
               return (
-                <div key={index} css={bp({ display: "flex", padding: 44 })}>
+                <div
+                  key={index}
+                  css={bp({
+                    ...relative,
+                    zIndex: index == 0 ? 6 : 0,
+                    display: "flex",
+                    padding: 44,
+                    marginBottom: notMobile ? 0 : index == 0 ? "64%" : 0,
+                  })}>
                   <div css={bp({})}>
                     <div>{index}</div>
                     {section.blocks.map((block, index) => (
